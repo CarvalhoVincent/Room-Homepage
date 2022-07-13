8ce9ffebe6bae7117d09caf61504af1b4b74d716
CarvalhoVincent <carvalho.vincent@hotmail.fr>
2022-07-13 19:47:46 +0200

    responsive ok

--- index.js
+++ index.js
@@ -1,26 +1,21 @@
 function displayMenu() {
     const menu = document.getElementById("primary-navigation");
     const state = document.querySelector(".mobile-nav-toggle");
-    const main = document.querySelector("main");
     const footer = document.querySelector("footer");
     menu.classList.toggle("primary-navigation-toggle");
 
     if(state.getAttribute("data-visible") === "false") {
-
-        state.style = "background-image: url(./images/icon-close.svg);";
+        menu.style = "box-shadow: 0px 0px 200px 100vh rgba(0,0,0,0.5);"
+        state.style = "background-image: url(./images/icon-close.svg); position: fixed;";
         state.setAttribute("data-visible", true);
         state.setAttribute("aria-expanded", true);
-        main.style.background = "#000000";
-        main.style.opacity = ".5";
         footer.style.background = "#000000";
         footer.style.opacity = ".5";
     } else {
-
-        state.style = "background-image: url(./images/icon-hamburger.svg);";
+        menu.style = "box-shadow: none;"
+        state.style = "background-image: url(./images/icon-hamburger.svg); position: absolute;";
         state.setAttribute("data-visible", false);
         state.setAttribute("aria-expanded", false);
-        main.style.background = "";
-        main.style.opacity = "";
         footer.style.background = "";
         footer.style.opacity = "";
     }
